e160c6268ad938ae6731be27f6b67471752bc354
vagatta <diego.sanmiguel.delpozo1314@gmail.com>
2026-03-09 23:43:21 +0100

    Redesign pricing cards, update footer and add privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://newfitgimnasio.com/',
+    site: 'https://newfit.es/',
     integrations: [sitemap()]
 });
